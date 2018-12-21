5b8e195d5a7e7a0b9169381e1b032c2d218ad341
sfeek <shane.feek@gmail.com>
2018-12-21 16:33:55 -0500

    Linted

--- tokenizer.c
+++ tokenizer.c
@@ -1,12 +1,12 @@
 #include "tokenizer.h"
 
-/* Function to split a string into an array of strings by a delimiter 
+/* Function to split a string into an array of strings by a delimiter
 	Input:
 		char *[]: to a preallocated array of character pointers
 		char *: to buffer containing string to be tokenized
 		int: max number of available elements in the token array
 		char *: delimiter string
-	
+
 	Output:
 		char *[] filled with tokens from the string and a return count of the number of elements filled on success
 		returns MAX_TOKENS_REACHED if there are more tokens available than space in the token array to put them
@@ -15,65 +15,66 @@
 		if there is a NULL buffer, NULL_BUFFER is returned and nothing is processed
 */
 
-int tokenize(char *single[],char *input,int max_tokens,char *delimiter) {
-	char *token;
-	int index = 0;
-	int i;
-	int token_flag;
-	char *buffer;
+int tokenize (char *single[], char *input, int max_tokens, char *delimiter)
+{
+    char *token;
+    int index = 0;
+    int i;
+    int token_flag;
+    char *buffer;
 
-	if (single == NULL || input == NULL || delimiter == NULL) return NULL_BUFFER;/* Handle NULL parameters */
+    if (single == NULL || input == NULL || delimiter == NULL) return NULL_BUFFER;/* Handle NULL parameters */
 
-	if (max_tokens < 1) return MAX_TOKENS_REACHED;/* Make sure that we have at least one token */
+    if (max_tokens < 1) return MAX_TOKENS_REACHED;/* Make sure that we have at least one token */
 
-	max_tokens++;
+    max_tokens++;
 
-	buffer = strdup(input); /* Make a copy of the string to be tokenized so that the original is not destroyed */
+    buffer = strdup (input); /* Make a copy of the string to be tokenized so that the original is not destroyed */
 
-	token = strtok(buffer,delimiter); /* Get the first token */
+    token = strtok (buffer, delimiter); /* Get the first token */
 
-	index = 0; /* Reset index counter */
-	token_flag = OK; /* Reset token flag */
+    index = 0; /* Reset index counter */
+    token_flag = OK; /* Reset token flag */
 
-	while(token != NULL) /* Cycle through until the end of the buffer is reached */
-	{
-		single[index] = malloc(strlen(token)+1); /* Make room for a copy of the token, including null terminator */
+    while (token != NULL) /* Cycle through until the end of the buffer is reached */
+    {
+        single[index] = malloc (strlen (token) + 1); /* Make room for a copy of the token, including null terminator */
 
-		if (single[index] == NULL) /* Check if memory allocation failed, if so flag and leave the loop  */ 
-		{
-			token_flag = MEMORY_ERROR;
-			break;
-		}
-		
-		strcpy(single[index],token); /* Make copy of the token */
+        if (single[index] == NULL) /* Check if memory allocation failed, if so flag and leave the loop  */
+        {
+            token_flag = MEMORY_ERROR;
+            break;
+        }
 
-		token = strtok(NULL,delimiter);  /* Get the next token */
-		
-		index++; /* Increment the index */
+        strcpy (single[index], token); /* Make copy of the token */
 
-		if (index == max_tokens) /* Make sure that we do not exceed token array count, if so flag and leave the loop */
-		{
-			token_flag = MAX_TOKENS_REACHED;
-			break; 		
-		}
-	}
+        token = strtok (NULL, delimiter); /* Get the next token */
 
-	/* Check token result flags, cleanup and return if flags tripped */
-	free (buffer); /* Free the copy string */
+        index++; /* Increment the index */
 
-	if (token_flag == MAX_TOKENS_REACHED)
-	{
-		for(i = 0;i < index;i++) free(single[i]);
-		
-		return MAX_TOKENS_REACHED;
-	}
+        if (index == max_tokens) /* Make sure that we do not exceed token array count, if so flag and leave the loop */
+        {
+            token_flag = MAX_TOKENS_REACHED;
+            break;
+        }
+    }
 
-	if (token_flag == MEMORY_ERROR) 
-	{
-		for(i = 0;i < index;i++) free(single[i]);
+    /* Check token result flags, cleanup and return if flags tripped */
+    free (buffer); /* Free the copy string */
 
-		return MEMORY_ERROR;
-	}
-	
-	return index; /* Return number of strings created if successful */
+    if (token_flag == MAX_TOKENS_REACHED)
+    {
+        for (i = 0; i < index; i++) free (single[i]);
+
+        return MAX_TOKENS_REACHED;
+    }
+
+    if (token_flag == MEMORY_ERROR)
+    {
+        for (i = 0; i < index; i++) free (single[i]);
+
+        return MEMORY_ERROR;
+    }
+
+    return index; /* Return number of strings created if successful */
 }
